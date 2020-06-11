d538dd5528159733bd27401b40caaf4098fb7dd5
Rohit Ambre <rohit003.ambre@gmail.com>
2020-06-12 00:08:06 +0530

    added winston logger config and README

---
+++ README.md
@@ -0,0 +1,14 @@
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+
+# node-auth-mysql
+____
+Node(Express), MySql based app with Authentication Boilerplate.\
+It can be used in the start of any project to have JWT authentication.
+
+
+## How to install
+
+1. clone the repo
+2. run ```npm install```
+3. create ```.env``` file as given in ```.env.example```
+4. run ```npm run dev```
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// Modules
+const logger = require('./winston-config');
+
 require('dotenv').config();
 
 // create express app
@@ -13,6 +16,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
 
+if (process.env.NODE_ENV !== 'production') {
+  // app.use(morgan('dev'));
+  app.use(
+    morgan('dev', {
+      stream: logger.stream,
+      // only log error responses
+      // skip: (req, res) => {
+      //     return res.statusCode < 400;
+      // },
+    })
+  );
+}
+
 app.listen(process.env.NODE_PORT, () => {
-  console.log(`server started on port ${process.env.NODE_PORT}`);
+  logger.info(`server started on port ${process.env.NODE_PORT}`);
 });
